d7435261e493e1bd64c04837e30f4ed0c103ab06
Shami3166 <sahtisham928@gmail.com>
2025-09-07 12:44:39 +0500

    updated

--- src/controllers/newsletterController.ts
+++ src/controllers/newsletterController.ts
@@ -51,7 +51,7 @@ export const subscribeNewsletter = async (req: Request, res: Response) => {
       subscription,
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error });
+    res.status(500).json( error );
   }
 };
 
